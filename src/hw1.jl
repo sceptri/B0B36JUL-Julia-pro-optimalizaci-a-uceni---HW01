6e731cdd142beba81fa457ce9f3cf4e6d8772ee0
sceptri <stepan.zapadlo@gmail.com>
2022-10-19 13:34:44 +0200

    Removed recursion

--- src/hw1.jl
+++ src/hw1.jl
@@ -14,29 +14,30 @@ function findroot(
     a::Real,
     b::Real;
     atol=1e-8,
-    maxiter=1000,
-    curiter=1
+    maxiter=1000
 )
-    # We do not need to consider case a = b as it will cause a DomainError later
-    if a > b
-        a, b = b, a
+    c = a
+    current_iter = 1
+    while abs(f(c)) > atol && current_iter < maxiter
+        # We do not need to consider case a = b as it will cause a DomainError later
+        if a > b
+            a, b = b, a
+        end
+
+        # If the root was supplied as one the endpoints, return it
+        if abs(f(a)) < atol
+            return a
+        elseif abs(f(b)) < atol
+            return b
+        end
+
+        sign(f(a)) == sign(f(b)) && throw(DomainError((a, b), "endpoints of the interval must have oposite signs"))
+
+        c = midpoint(method, f, a, b)
+
+        a, b = sign(f(a)) == sign(f(c)) ? (c, b) : (a, c)
+        current_iter += 1
     end
 
-    # If the root was supplied as one the endpoints, return it
-    if abs(f(a)) < atol
-        return a
-    elseif abs(f(b)) < atol
-        return b
-    end
-
-    sign(f(a)) == sign(f(b)) && throw(DomainError((a, b), "endpoints of the interval must have oposite signs"))
-
-    c = midpoint(method, f, a, b)
-
-    if abs(f(c)) < atol || curiter > maxiter
-        return c
-    else
-        aₙ, bₙ = sign(f(a)) == sign(f(c)) ? (c, b) : (a, c)
-        return findroot(method, f, aₙ, bₙ; atol, maxiter, curiter=curiter + 1)
-    end
+    return c
 end
